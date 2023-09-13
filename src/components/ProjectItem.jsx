0bf673e29f70b8b72d1210e1268fc27803788ac2
bemrekalkan <bemre.kalkan@gmail.com>
2023-09-13 14:04:58 +0300

    Projects page updated

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const ProjectItem = ({ image, name }) => {
+    const redirectToImageLink = () => {
+        window.open(image, '_blank'); // Opens the specified URL in a new tab.
+    };
+
     return (
-        <div className='projectItem'>
+        <div className='projectItem' onClick={redirectToImageLink}>
             <div className="picture">
                 <iframe src={image} title={name}></iframe>
             </div>
             <h3>{name}</h3>
         </div>
-    )
-}
+    );
+};
 
-export default ProjectItem
+export default ProjectItem;
